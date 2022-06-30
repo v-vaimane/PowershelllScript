133f2204890b867ad02c902e668b3020f0e00413
v-vaimane <106580830+v-vaimane@users.noreply.github.com>
2022-06-30 11:26:13 +0530

    Update StreamJob_start.ps1

--- StreamJob_start.ps1
+++ StreamJob_start.ps1
@@ -1,12 +1,14 @@
 
 param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName)
 
-Connect-AzAccount -UseDeviceAuthentication
+$context = Set-AzContext -Subscription $SubscriptionId
+
+Connect-AzAccount -ContextName $context
+
 Get-AzStreamAnalyticsJob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -Name $JobName | Start-AzStreamAnalyticsJob
 
 
 
 
-#Get-AzStreamAnalyticsJob -SubscriptionId '995f2d66-72ff-4037-92c3-1a72fddac360' -ResourceGroupName 'RG_290622_02' -Name 'RG_290622_02e2wg2ymkm6oiu' | Start-AzStreamAnalyticsJob
 
 
