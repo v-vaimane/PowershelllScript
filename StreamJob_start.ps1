816471c267a6aa10d9a8010caecf06efe164a94e
v-vaimane <106580830+v-vaimane@users.noreply.github.com>
2022-06-30 18:26:49 +0530

    Update StreamJob_start.ps1

--- StreamJob_start.ps1
+++ StreamJob_start.ps1
@@ -7,7 +7,7 @@ $SubscriptionId = '995f2d66-72ff-4037-92c3-1a72fddac360'
 $User = 'william@PSA365.onmicrosoft.com'
 $PWord = ConvertTo-SecureString -String "Fpsteam1" -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User,$PWord
-$TenantId = (Get-AzContext).Tenant.Id
+$TenantId = 'f782ab25-637c-4888-b38e-79c78d14bb50'
 
 Connect-AzAccount -Credential $Credential -Tenant $TenantId -Subscription $SubscriptionId
 
