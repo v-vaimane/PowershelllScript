1e4a85d3b928a0774096c3f0e3398c1cc0f5190a
v-vaimane <106580830+v-vaimane@users.noreply.github.com>
2022-06-30 17:39:57 +0530

    Update StreamJob_start.ps1

--- StreamJob_start.ps1
+++ StreamJob_start.ps1
@@ -1,8 +1,18 @@
 
 param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName)
 
+$ResourceGroupName = 'RG_300622_01'
+$JobName ='RG_300622_016m7y6ooiyvrey'
+$SubscriptionId = '995f2d66-72ff-4037-92c3-1a72fddac360'
+$accountid = 'william@PSA365.onmicrosoft.com'
+$TenantId = 'f782ab25-637c-4888-b38e-79c78d14bb50'
+
+$context = Set-AzContext -Subscription $SubscriptionId
+
+Connect-AzAccount -ContextName $context -TenantId $TenantId -SubscriptionId $SubscriptionId
 
 Get-AzStreamAnalyticsJob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -Name $JobName | Start-AzStreamAnalyticsJob
+
 
 
 
