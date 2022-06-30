4fc9fef692daf2201dcc44e67277f0fe765c02ef
v-vaimane <106580830+v-vaimane@users.noreply.github.com>
2022-06-30 12:34:15 +0530

    Update StreamJob_start.ps1

--- StreamJob_start.ps1
+++ StreamJob_start.ps1
@@ -2,8 +2,6 @@
 param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName)
 
 
-Connect-AzAccount -Subscription $SubscriptionId
-
 Get-AzStreamAnalyticsJob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -Name $JobName | Start-AzStreamAnalyticsJob
 
 
