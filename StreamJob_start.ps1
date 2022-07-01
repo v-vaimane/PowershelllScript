44690d896c4a65ea4d2d2e6ae52221cb1a5cb5a7
v-vaimane <106580830+v-vaimane@users.noreply.github.com>
2022-07-01 09:32:34 +0530

    Update StreamJob_start.ps1

--- StreamJob_start.ps1
+++ StreamJob_start.ps1
@@ -1,13 +1,12 @@
 
-param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName)
+param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName,[string]$TenantId)
 
-$ResourceGroupName = 'RG_300622_01'
-$JobName ='RG_300622_016m7y6ooiyvrey'
-$SubscriptionId = '995f2d66-72ff-4037-92c3-1a72fddac360'
+$ResourceGroupName = 'RG_300622_02'
+$JobName ='RG_300622_02bskmkujmlarv2-PBI'
+
 $User = 'william@PSA365.onmicrosoft.com'
 $PWord = ConvertTo-SecureString -String "Fpsteam1" -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User,$PWord
-$TenantId = 'f782ab25-637c-4888-b38e-79c78d14bb50'
 
 Connect-AzAccount -Credential $Credential -Tenant $TenantId -Subscription $SubscriptionId
 
