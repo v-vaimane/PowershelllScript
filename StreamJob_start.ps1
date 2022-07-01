1ace8f4132b5622fcec911cdc0d9ba27d259c32c
v-vaimane <106580830+v-vaimane@users.noreply.github.com>
2022-07-01 13:50:24 +0530

    Update StreamJob_start.ps1

--- StreamJob_start.ps1
+++ StreamJob_start.ps1
@@ -1,24 +1,12 @@
-
-param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName)
-
-
-$context = Set-AzContext -Subscription $SubscriptionId
-$User = $context.Account.Id
-$TenantId = $context.Tenant
-
-$JobName ='RG_300622_02bskmkujmlarv2-PBI'
+param([string]$SubscriptionId ,[string]$ResourceGroupName,[string]$JobName,[string]$TenantId)
 
 $User = 'william@PSA365.onmicrosoft.com'
 $PWord = ConvertTo-SecureString -String "Fpsteam1" -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User,$PWord
 
-Connect-AzAccount -Credential $Credential -Tenant $TenantId -Subscription $SubscriptionId
+Connect-AzAccount -Credential $Credential -Tenant $TenantId -SubscriptionId $SubscriptionId
 
 Get-AzStreamAnalyticsJob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -Name $JobName  | Start-AzStreamAnalyticsJob
-
-
-
-
 
 
 
